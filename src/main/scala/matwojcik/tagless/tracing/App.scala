692eecfd3d705ac8e53670b0da17941da6793185
Mateusz Wojcik <mateusz.wojcik@ocado.com>
2019-01-07 16:46:58 +0100

    Added parallel calls

--- src/main/scala/matwojcik/tagless/tracing/App.scala
+++ src/main/scala/matwojcik/tagless/tracing/App.scala
@@ -1,12 +1,15 @@
 package matwojcik.tagless.tracing
-import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.Executors
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
-import cats.effect.{Async, Concurrent, ExitCode, IO, IOApp, Resource, Sync}
+import cats.Parallel
+import cats.effect.{Async, ExitCode, IO, IOApp, Resource, Sync}
 import cats.syntax.flatMap._
 import cats.syntax.functor._
+import cats.instances.list._
+import cats.syntax.parallel._
 import com.typesafe.scalalogging.StrictLogging
 import io.chrisdavenport.log4cats.Logger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
@@ -18,7 +21,7 @@ import scala.concurrent.ExecutionContext
 
 object App extends IOApp with StrictLogging {
 
-  implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(new ForkJoinPool(5))
+  implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
   implicit def unsafeLogger[F[_]: Sync]: Logger[F] = Slf4jLogger.unsafeCreate[F]
   implicit val system: ActorSystem = ActorSystem()
   implicit val materializer: ActorMaterializer = ActorMaterializer()
@@ -29,7 +32,7 @@ object App extends IOApp with StrictLogging {
   val akkaHttpServer: HttpServer[IO, Http.ServerBinding] = AkkaHttpServer.instance(new AkkaHttpServer(Something[IO], RemoteCaller.akkaHttp))
 
   override def run(args: List[String]): IO[ExitCode] =
-    Program.program(http4sServer)(Id("123"))
+    Program.program(akkaHttpServer)(3)
 
 }
 
@@ -37,16 +40,17 @@ object Program {
 
   import cats.syntax.all._
 
-  def program[F[_]: Async: RemoteCaller: DeferFuture, T](server: HttpServer[F, T])(id: Id): F[ExitCode] =
-    buildAndTrigger(server, id).use(_ => Async[F].never[Unit]).as(ExitCode.Success)
+  def program[F[_]: Async: RemoteCaller: DeferFuture, G[_], T](server: HttpServer[F, T])(amountOfCalls: Int)(implicit p: Parallel[F,G]): F[ExitCode] =
+    buildAndTrigger(server, amountOfCalls).use(_ => Async[F].never[Unit]).as(ExitCode.Success)
 
-  private def buildAndTrigger[T, F[_]: Async: RemoteCaller: DeferFuture](
+  private def buildAndTrigger[T, F[_]: Async: RemoteCaller: DeferFuture, G[_]](
     server: HttpServer[F, T],
-    id: Id
-  ): Resource[F, Unit] =
+    amountOfCalls: Int
+  )(implicit p: Parallel[F,G]): Resource[F, Unit] =
     for {
       _ <- server.build
-      _ <- Resource.liftF(RemoteCaller[F].callRemote(id, s"http://localhost:8080/trigger/${id.value}"))
+      ids = (1 to amountOfCalls).map(i => Id(i.toString)).toList
+      _ <- Resource.liftF(ids.map(id => RemoteCaller[F].callRemote(id, s"http://localhost:8080/trigger/${id.value}")).parSequence)
     } yield ()
 }
 
